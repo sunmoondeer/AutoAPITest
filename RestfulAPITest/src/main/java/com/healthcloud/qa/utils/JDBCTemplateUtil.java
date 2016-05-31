c3a5ab50c6ad84f8254a248b5305caac13a09383
jack <lusun7@creditease.cn>
2016-05-31 14:56:13 +0800

    Summary: Add get column name, row value, row type methods
    Description:
    getColumnName(String sql, JdbcTemplate mysqlJdbcTemplate):get column
    name
    
    getRowsValue(String sql, JdbcTemplate mysqlJdbcTemplate):get row value
    
    getRowsType(String sql, JdbcTemplate mysqlJdbcTemplate):get row type

--- RestfulAPITest/src/main/java/com/healthcloud/qa/utils/JDBCTemplateUtil.java
+++ RestfulAPITest/src/main/java/com/healthcloud/qa/utils/JDBCTemplateUtil.java
@@ -12,6 +12,7 @@ import java.util.Map;
 import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowCallbackHandler;
+import org.springframework.jdbc.core.RowCountCallbackHandler;
 import org.springframework.jdbc.core.RowMapper;
 
 public class JDBCTemplateUtil {
@@ -62,8 +63,60 @@ public class JDBCTemplateUtil {
 		
 		return mysqlJdbcTemplate;
 	}
-		
-		
+	/*
+	 * Get results columns name	
+	 */
+	public static String[] getColumnName(String sql, JdbcTemplate mysqlJdbcTemplate) {
+		RowCountCallbackHandler rcch = new RowCountCallbackHandler();
+		mysqlJdbcTemplate.query(sql,rcch);
+		String[] str= rcch.getColumnNames();
+		return str;
+	}	
+	
+	public static String[] getRowsValue(String sql, JdbcTemplate mysqlJdbcTemplate) {
+		RowCountCallbackHandler rcch = new RowCountCallbackHandler();
+		mysqlJdbcTemplate.query(sql,rcch);
+		String[] str= rcch.getColumnNames();
+		Map result = mysqlJdbcTemplate.queryForMap(sql);
+		for(int i = 0;i<str.length;i++){
+			  System.out.println("Column name: " + str[i]);
+			  str[i] = result.get(str[i]).toString();
+	          System.out.println("Value: " + str[i]);
+	       }
+		return str;
+	}	
+	
+	public static int[] getRowsType(String sql, JdbcTemplate mysqlJdbcTemplate) {
+		RowCountCallbackHandler rcch = new RowCountCallbackHandler();
+		mysqlJdbcTemplate.query(sql,rcch);
+		int[] columnType = rcch.getColumnTypes();
+		Map result = mysqlJdbcTemplate.queryForMap(sql);
+		for(int i = 0;i<columnType.length;i++){
+			  System.out.println("Column type: " + columnType[i]);
+	       }
+		return columnType;
+	}	
 
+	public static void main(String args[]) {
+		String sql = "select name,parent_id from nodes_hierarchy where id = 47";
+		String dbUrl = "jdbc:mysql://" + "10.100.141.25" + ":" + "3306" + "/" + "testlink";
+		JdbcTemplate db = new JDBCTemplateUtil().getMySQLJdbcTemplate(dbUrl, "jman", "UIOWQ129jiqP");
+		/*List<Map> result = db.query(sql, new RowMapper<Map>() {  
 
+		      @Override  
+
+		      public Map mapRow(ResultSet rs, int rowNum) throws SQLException {  
+
+		          Map row = new HashMap();  
+
+		          row.put(rs.getInt("parent_id"), rs.getString("name"));  
+
+		          return row;  
+
+		  }});*/
+		
+		getRowsValue(sql, db);
+		getRowsType(sql, db);
+	}
+		
 }
